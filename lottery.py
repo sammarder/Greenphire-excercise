16d2541186d15357dd3384a08bcd5a35d00dbc2c
Sam Marder <smardercollege@gmail.com>
2017-03-24 23:51:41 -0400

    Adding in the powerball

--- lottery.py
+++ lottery.py
@@ -1,17 +1,17 @@
-def get_next(numbers, prompt):
+def get_next(numbers, prompt, max):
     next = raw_input(prompt.format(*numbers)) 
-    if (valid_number(numbers, next)):
+    if (valid_number(numbers, next, max)):
         return next 
-    while (not valid_number(numbers, next)): 
+    while (not valid_number(numbers, next, max)): 
         print("Please try again")
         next = raw_input(prompt.format(*numbers)) 
-        if (valid_number(numbers, next)):
+        if (valid_number(numbers, next, max)):
             return next
 
-def valid_number(numbers, next):
+def valid_number(numbers, next, max):
     try:
         x = int(next) 
-        if (x < 1 or x > 69):
+        if (x < 1 or x > max):
             return False 
         if (next in numbers):
             return False 
@@ -34,10 +34,21 @@ if __name__ == "__main__":
     last = get_name("Enter your last name: ") 
     numbers = [] 
 
-    numbers.append(get_next(numbers, "select 1st # (1 thru 69): ")) 
-    numbers.append(get_next(numbers, "select 2nd # (1 thru 69 excluding {0}): ")) 
-    numbers.append(get_next(numbers, "select 3rd # (1 thru 69 excluding {0} and {1}): ")) 
-    numbers.append(get_next(numbers, "select 4th # (1 thru 69 excluding {0}, {1} and {2}): ")) 
-    numbers.append(get_next(numbers, "select 5th # (1 thru 69 excluding {0}, {1}, {2} and {3}): ")) 
-    print(numbers) 
+    numbers.append(get_next(numbers, "select 1st # (1 thru 69): ", 69))
+    numbers.append(get_next(numbers, "select 2nd # (1 thru 69 excluding {0}): ", 69))
+    numbers.append(get_next(numbers, "select 3rd # (1 thru 69 excluding {0} and {1}): ", 69))
+    numbers.append(get_next(numbers, "select 4th # (1 thru 69 excluding {0}, {1} and {2}): ", 69))
+    numbers.append(get_next(numbers, "select 5th # (1 thru 69 excluding {0}, {1}, {2} and {3}): ", 69))
+    powerball = get_next([], "select Power Ball # (1 thru 26): ", 26) 
+    
+    result = first + " " + last + " {0}, {1}, {2}, {3}, {4} Powerball: ".format(*numbers) + str(powerball) + "\n"
 
+    output = open("lotto.txt", "a")
+    input = open("lotto.txt", "r")
+    for line in input:
+        print(line.strip())
+    input.close()
+    
+    output.write(result)
+    output.close()
+    print(result.strip())
